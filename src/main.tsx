42ab95114038cea4ed289f4cef055830c54fbe24
Dmitrijs Zaicevs <A230223DZ@rvt.lv>
2026-09-08 23:18:43 +0300

    Konfigurēt Firebase projektu un vides

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { auth, db, functions } from './firebase';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+console.log('Firebase initialized:', auth, db, functions);
